709f24fee761ea114ab464c5341428667c13295f
Norberto Bensa <nbensa@gmail.com>
2024-03-04 13:11:40 -0300

    challenge #1: find all sites

--- redisolar/dao/redis/site.py
+++ redisolar/dao/redis/site.py
@@ -27,7 +27,8 @@ class SiteDaoRedis(SiteDaoBase, RedisDaoBase):
     def find_by_id(self, site_id: int, **kwargs) -> Site:
         """Find a Site by ID in Redis."""
         hash_key = self.key_schema.site_hash_key(site_id)
-        site_hash = self.redis.hgetall(hash_key)
+        client = kwargs.get('pipeline', self.redis)
+        site_hash = client.hgetall(hash_key)
 
         if not site_hash:
             raise SiteNotFound()
@@ -37,8 +38,8 @@ class SiteDaoRedis(SiteDaoBase, RedisDaoBase):
     def find_all(self, **kwargs) -> Set[Site]:
         """Find all Sites in Redis."""
         # START Challenge #1
-        # Remove this line when you've written code to build `site_hashes`.
-        site_hashes = []  # type: ignore
+        site_ids_key = self.key_schema.site_ids_key()
+        client = kwargs.get('pipeline', self.redis)
+        site_ids = client.smembers(site_ids_key)
+        return {self.find_by_id(site_id) for site_id in site_ids}
         # END Challenge #1
-
-        return {FlatSiteSchema().load(site_hash) for site_hash in site_hashes}
